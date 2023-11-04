020cee4588ef44395ca0d2ecd9088a50bdd2ae53
frodo821 <sakaic2003@gmail.com>
2023-11-04 21:17:25 +0900

    サービスとして登録するコマンドを追加

--- src/settings.ts
+++ src/settings.ts
@@ -5,7 +5,7 @@ import { join } from "path";
 export const DISCORD_APP_ID = process.env.DISCORD_APP_ID;
 export const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
 export const APP_NAME = 'picsy';
-export const DATA_DIR = join(homedir(), `.${APP_NAME}`);
+export const DATA_DIR = process.env.PICSY_ALTERNATE_DATA_DIRECTORY || join(homedir(), `.${APP_NAME}`);
 
 
 if (!existsSync(DATA_DIR)) {
